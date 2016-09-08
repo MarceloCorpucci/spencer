164a8c95f771b5035ca99e35eaab9112afe9ee84
marcelo-corpucci <mcorpucci@velocitypartners.net>
2016-09-08 12:27:06 -0300

    code to make run basic test.

---
+++ spencer.py
@@ -0,0 +1,10 @@
+from errbot import BotPlugin, botcmd
+
+
+class Spencer(BotPlugin):
+	"""Spencer basic plugin for Errbot"""
+
+    @botcmd
+    def feature(self, args):
+    	"""Minimal implementation"""
+        return "Got it, don't forget the feature name."
