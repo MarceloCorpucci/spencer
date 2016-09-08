ef1cbb7253d947c77fe2487d841518865f2d98cb
marcelo-corpucci <mcorpucci@velocitypartners.net>
2016-09-08 11:54:28 -0300

    initial commit.

---
+++ test_spencer.py
@@ -0,0 +1,10 @@
+import os
+from errbot.backends.test import testbot
+
+
+class TestSpencer(object):
+    extra_plugin_dir = '.'
+
+    def test_listen(self, testbot):
+        testbot.push_message('!Feature')
+        assert "Got it, don't forget the feature name." in testbot.pop_message()
