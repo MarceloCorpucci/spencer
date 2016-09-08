30aaebf99a528399bc5f9bd551351a2b7391f54c
marcelo-corpucci <mcorpucci@velocitypartners.net>
2016-09-08 13:05:51 -0300

    Feature method was changed to feature due to a botcmd errboth command.

--- spencer.py
+++ spencer.py
@@ -5,6 +5,6 @@ class Spencer(BotPlugin):
     """Spencer basic plugin for Errbot"""
 
     @botcmd
-    def Feature(self, args):
+    def feature(self, args):
         """Minimal implementation"""
         return "Got it, don't forget the feature name."
--- test_spencer.py
+++ test_spencer.py
@@ -6,5 +6,5 @@ class TestSpencer(object):
     extra_plugin_dir = '.'
 
     def test_listen(self, testbot):
-        testbot.push_message('!Feature')
+        testbot.push_message('!feature')
         assert "Got it, don't forget the feature name." in testbot.pop_message()
